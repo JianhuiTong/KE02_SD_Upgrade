196dd2fc68e2dad002691f19df1d104b05366bf4
Jianhui.Tong <jianhui.tong@outlook.com>
2016-11-21 16:07:42 +0800

    Modified the README.md file.

--- KEXX_DRIVERS_V1.2.1_DEVD/kexx_drv_lib/build/iar/ke02/SD_Upgrade/settings/SD_Upgrade_frdm.FLASH_32KB_PFLASH.cspy.bat
+++ KEXX_DRIVERS_V1.2.1_DEVD/kexx_drv_lib/build/iar/ke02/SD_Upgrade/settings/SD_Upgrade_frdm.FLASH_32KB_PFLASH.cspy.bat
@@ -25,7 +25,7 @@ if not "%1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\b55908\Desktop\FatfsPorting\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.general.xcl" --backend -f "C:\Users\b55908\Desktop\FatfsPorting\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\b55908\Documents\GIT\test\KE02_SD_Upgrade\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.general.xcl" --backend -f "C:\Users\b55908\Documents\GIT\test\KE02_SD_Upgrade\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\b55908\Desktop\FatfsPorting\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.general.xcl" "--debug_file=%1" --backend -f "C:\Users\b55908\Desktop\FatfsPorting\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "C:\Users\b55908\Documents\GIT\test\KE02_SD_Upgrade\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.general.xcl" "--debug_file=%1" --backend -f "C:\Users\b55908\Documents\GIT\test\KE02_SD_Upgrade\KEXX_DRIVERS_V1.2.1_DEVD\kexx_drv_lib\build\iar\ke02\SD_Upgrade\settings\SD_Upgrade_frdm.FLASH_32KB_PFLASH.driver.xcl" 
 
 @echo off 
 :end
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KE02_SD_Upgrade
-A SD Upgrade Demo based on FRDM-KE02Z40M. 
+A SD Upgrade Demo based on FRDM-KE02Z40M. More information please refer to "基于KE02的SD卡升级Demo".
+
+IAR version
+7.4
